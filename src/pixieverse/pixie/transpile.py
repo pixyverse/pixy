66b3ff25225c1577ab45c570def73560103469d1
Pradeep Roark <pradeep.roark@gmail.com>
2024-05-17 18:56:07 +0100

    Use pre-generated Pixie Parser module

--- src/pixieverse/pixie/transpile.py
+++ src/pixieverse/pixie/transpile.py
@@ -1,10 +1,13 @@
 import ast
-from pixieverse.pixie.genparser import generatePixieParserModule
+
+"""This is a reference to a generated parser. Generate it using
+python -m pegen ./src/pixieverse/pixie/grammar/pypixie.gram -o src/pixieverse/pixie/parser.py
+"""
+from pixieverse.pixie.parser import parse_string
 
 
 def transpile_source_tomodule(source: str) -> ast.Module:
-    module = generatePixieParserModule()
-    return module.parse_string(source, mode="exec")
+    return parse_string(source, mode="exec")
 
 
 def transpile_source(source: str) -> str:
